c17b82009ab570083df0d2f833b684e2605044f2
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2024-04-17 09:34:23 +0600

    add login form

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import "antd/dist/reset.css"
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router.tsx'
+import { ConfigProvider } from 'antd'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ConfigProvider theme={{token: {colorPrimary: '#f65f42', colorLink: '#f65f42'}}}>
     <RouterProvider router={router} />
+    </ConfigProvider>
   </React.StrictMode>,
 )
