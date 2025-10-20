a603d7d5981954a285263a12ec9fd2501fa60464
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-10-20 20:20:19 +0600

    add self endpoint

--- src/http/api.ts
+++ src/http/api.ts
@@ -2,3 +2,4 @@ import type { Credentials } from "../types";
 import { api } from "./client";
 
 export const login = (credentials: Credentials) => api.post('/auth/login', credentials);
+export const self = () => api.get('/auth/self');
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,20 +1,34 @@
 import { Layout, Card, Space, Form, Input, Checkbox, Button, Flex, Alert } from "antd"
 import {LockFilled, LockOutlined, UserOutlined} from '@ant-design/icons';
 import Logo from "../../components/icons/Logo";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import type { Credentials } from "../../types";
-import { login } from "../../http/api";
+import { login, self } from "../../http/api";
 
 const loginUser = async(credentials: Credentials) => {
   const data = await login(credentials);
   return data;
 };
 
+const getSelf = async() => {
+  const {data} = await self();
+  return data;
+}
+
 const LoginPage = () => {
+
+  const {data: selfData, refetch} = useQuery({
+    queryKey: ['self'],
+    queryFn: getSelf,
+    enabled: false,
+  });
+
   const {mutate, isPending, isError, error} = useMutation({
     mutationKey: ['login'],
     mutationFn: loginUser,
     onSuccess: async () => {
+      refetch();
+      console.log('userdata', selfData);
       console.log('Login successful')
     }
   });
@@ -26,26 +40,26 @@ const LoginPage = () => {
           <Logo/>
           </Layout.Content>
 
-        <Card variant="borderless" style={{width: 300}} title={<Space style={{width: '100%', fontSize: 16, justifyContent: 'center'}}><LockFilled/> Sign in</Space>}>
-          <Form initialValues={{remember: true}} onFinish={(values) => {mutate({email: values.username, password: values.password}); console.log(values)}}>
-            {isError && <Alert style={{marginBottom: 24}} type="error" message={error.message}/>}
-            <Form.Item name="username" rules={[{required: true, message: 'Please input your Username'}, {type: 'email', message: 'Email is not valid'}]}>
-              <Input prefix={<UserOutlined/>} placeholder="Username"/>
-            </Form.Item>
-            <Form.Item name="password" rules={[{required: true, message: 'Please input your Password'}]}>
-              <Input.Password prefix={<LockOutlined/>} placeholder="Password"/>
-            </Form.Item>
-            <Flex justify="space-between">
-            <Form.Item name="remember" valuePropName='checked'>
-              <Checkbox>Remember me</Checkbox>
-            </Form.Item>
-              <a href="" id="login-form-forgot">Forgot password</a>
-            </Flex>
-            <Form.Item>
-              <Button type="primary" htmlType="submit" style={{width: '100%'}} loading={isPending}>Login</Button>
-            </Form.Item>
-          </Form>
-        </Card>
+          <Card variant="borderless" style={{width: 300}} title={<Space style={{width: '100%', fontSize: 16, justifyContent: 'center'}}><LockFilled/> Sign in</Space>}>
+            <Form initialValues={{remember: true}} onFinish={(values) => {mutate({email: values.username, password: values.password}); console.log(values)}}>
+              {isError && <Alert style={{marginBottom: 24}} type="error" message={error.message}/>}
+              <Form.Item name="username" rules={[{required: true, message: 'Please input your Username'}, {type: 'email', message: 'Email is not valid'}]}>
+                <Input prefix={<UserOutlined/>} placeholder="Username"/>
+              </Form.Item>
+              <Form.Item name="password" rules={[{required: true, message: 'Please input your Password'}]}>
+                <Input.Password prefix={<LockOutlined/>} placeholder="Password"/>
+              </Form.Item>
+              <Flex justify="space-between">
+              <Form.Item name="remember" valuePropName='checked'>
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+                <a href="" id="login-form-forgot">Forgot password</a>
+              </Flex>
+              <Form.Item>
+                <Button type="primary" htmlType="submit" style={{width: '100%'}} loading={isPending}>Login</Button>
+              </Form.Item>
+            </Form>
+          </Card>
       </Space>
     </Layout>
   </>
