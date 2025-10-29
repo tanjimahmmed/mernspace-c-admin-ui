127ade7cb19dd374172a468571bd53219641351d
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-10-29 19:26:21 +0600

    add tenants table and drawer

---
+++ src/pages/tenants/Tenants.tsx
@@ -0,0 +1,76 @@
+import { Breadcrumb, Button, Drawer, Space, Table } from 'antd'
+import React from 'react'
+import {PlusOutlined, RightOutlined} from '@ant-design/icons'
+import { Link, Navigate } from 'react-router-dom'
+import { useQuery } from '@tanstack/react-query'
+import { getTenants } from '../../http/api'
+import { useAuthStore } from '../../store'
+import TenantFilter from './TenantFilter'
+
+const columns = [
+    {
+        title: 'ID',
+        dataIndex: 'id',
+        key: 'id'
+    },
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name'
+    },
+    {
+        title: 'Address',
+        dataIndex: 'address',
+        key: 'address'
+    }
+]
+
+
+const Tenants = () => {
+const [drawerOpen, setDrawerOpen] = React.useState(false);
+const {data: tenants, isLoading, isError, error} = useQuery({
+    queryKey: ['tenants'], 
+    queryFn: () => {
+        return getTenants().then((res) => res.data)
+    }
+});
+
+const {user} = useAuthStore();
+
+if(user?.role !== 'admin'){
+    return <Navigate to='/' replace={true}/>;
+}
+
+  return (
+    <>
+        <Space direction='vertical' size='large' style={{width: '100%'}}>
+            <Breadcrumb 
+                separator={<RightOutlined/>}
+                items={[{title: <Link to="/">Dashboard</Link>}, {title: 'Tenants'}]}
+            />
+            {isLoading && <div>Loading...</div>}
+            {isError && <div>{error.message}</div>}
+
+            <TenantFilter onFilterChange={(filterName: string, filterValue: string) => {
+                console.log(filterName, filterValue)
+            }}>
+                <Button 
+                type='primary' 
+                icon={<PlusOutlined/>} 
+                onClick={() => setDrawerOpen(true)}>
+                    Add Restaurant
+                </Button>
+            </TenantFilter>
+
+            <Table columns={columns} dataSource={tenants} rowKey={'id'}/>
+
+            <Drawer title="Create restaurant" width={720} destroyOnHidden={true} open={drawerOpen} onClose={() => {setDrawerOpen(false);
+            }} extra={<Space><Button>Cancel</Button><Button type="primary">Submit</Button></Space>}>
+            <p>Some content...</p>
+            </Drawer>
+        </Space>
+    </>
+  )
+}
+
+export default Tenants
